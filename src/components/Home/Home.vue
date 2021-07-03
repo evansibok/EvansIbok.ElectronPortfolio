ef7c6448ce6b7a027a83afa466a237595599d9b5
Evans Ibok <evansibok@gmail.com>
2021-07-03 12:06:40 +0100

    handle name input values form

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -2,23 +2,43 @@
   <div class="w-flex">
     <div class="xs6 first-section flex align-center justify-center pt5">
       <h1>User Details</h1>
-      <h3 class="mt3">Hello {{ userDetails.firstName }}</h3>
+      <h3 class="mt3">Hello {{ state.user.firstName }}</h3>
 
       <w-form
         class="user-form justify-center align-center mt10 pa12"
         bg-color="primary"
       >
-        <w-input class="mb8" label="Label" placeholder="Enter First Name" />
-        <w-input class="mb3" label="Label" placeholder="Enter Last Name" />
+        <w-input
+          class="mb8"
+          label="First Name"
+          name="firstName"
+          placeholder="Enter First Name"
+          v-model="state.inputValues.firstName"
+        />
+        <w-input
+          class="mb3"
+          label="Last Name"
+          name="lastName"
+          placeholder="Enter Last Name"
+          v-model="state.inputValues.lastName"
+        />
 
-        <w-button class="mt8 pa5 bdrs5" bg-color="success"
-          >Submit Name</w-button
+        <w-button
+          class="name-btn mt8 pa5 bdrs5"
+          bg-color="success"
+          @click.prevent.stop="generateCode"
         >
+          <p v-if="state.loading === true">Loading...</p>
+          <p v-if="state.loading === false">Generate</p>
+        </w-button>
       </w-form>
 
-      <div></div>
-
-      <vue-qrcode value="Hello, World!" :options="{ width: 200 }"></vue-qrcode>
+      <div v-if="Object.values(state.qrcode).length !== 0">
+        <vue-qrcode
+          value="Hello, World!"
+          :options="{ width: 200 }"
+        ></vue-qrcode>
+      </div>
     </div>
 
     <div class="xs6 second-section flex align-center justify-center pt5">
@@ -33,13 +53,47 @@
 <script>
 export default {
   name: "Home",
+  props: {
+    value: {
+      type: String,
+    },
+  },
   data: () => {
     return {
-      userDetails: {
-        firstName: "Sam",
-        lastName: "Ben",
+      state: {
+        loading: false,
+        inputValues: {
+          firstName: "",
+          lastName: "",
+        },
+        user: {
+          firstName: "",
+          lastName: "",
+        },
+        qrcode: {},
       },
     };
+  },
+  methods: {
+    generateCode() {
+      // Function to generate QR Code for user details
+      console.log("state", this.state);
+      this.state.loading = true;
+      console.log(Object.values(this.state.qrcode).length === 0); // ["", ""]
+      console.log(Object.values(this.state.qrcode));
+      console.log(this.state.qrcode);
+
+      // Generate QR Code here
+
+
+      // Empty form fields
+      // this.state.inputValues = {
+      //   firstName: "",
+      //   lastName: "",
+      // };
+      // Stop loading
+      this.state.loading = false;
+    },
   },
 };
 </script>
@@ -54,5 +108,8 @@ export default {
 .user-form {
   width: 500px;
   margin: 0 auto;
+}
+.name-btn {
+  width: 150px;
 }
 </style>
