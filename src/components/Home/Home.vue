7c4896d28d3f4626203bb8932a57cd4f68ab004e
Evans Ibok <evansibok@gmail.com>
2021-07-03 16:46:46 +0100

    resolve issue with qrcode state in user details

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,79 +1,74 @@
 <template>
   <div class="w-flex">
     <!-- User Details Component Template -->
-    <!-- <template> -->
-      <div class="xs6 first-section flex align-center justify-center pt5">
-        <h1>User Details</h1>
-        <h3 class="mt3">Hello {{ state.user.firstName }}</h3>
+    <div class="xs6 first-section flex align-center justify-center pt5">
+      <h1>User Details</h1>
+      <h3 class="mt3">Hello {{ state.user.firstName }}</h3>
 
-        <w-form
-          class="user-form justify-center align-center mt10 pa12"
-          bg-color="primary"
+      <w-form
+        class="user-form justify-center align-center mt10 pa12"
+        bg-color="primary"
+      >
+        <w-input
+          class="mb8"
+          label="First Name"
+          name="firstName"
+          placeholder="Enter First Name"
+          v-model="state.inputValues.firstName"
+        />
+        <w-input
+          class="mb3"
+          label="Last Name"
+          name="lastName"
+          placeholder="Enter Last Name"
+          v-model="state.inputValues.lastName"
+        />
+
+        <w-button
+          class="name-btn mt8 pa5 bdrs5"
+          bg-color="success"
+          @click.prevent.stop="generateCode"
         >
-          <w-input
-            class="mb8"
-            label="First Name"
-            name="firstName"
-            placeholder="Enter First Name"
-            v-model="state.inputValues.firstName"
-          />
-          <w-input
-            class="mb3"
-            label="Last Name"
-            name="lastName"
-            placeholder="Enter Last Name"
-            v-model="state.inputValues.lastName"
-          />
+          <p v-if="state.loading === true">Loading...</p>
+          <p v-if="state.loading === false">Generate</p>
+        </w-button>
+      </w-form>
 
-          <w-button
-            class="name-btn mt8 pa5 bdrs5"
-            bg-color="success"
-            @click.prevent.stop="generateCode"
-          >
-            <p v-if="state.loading === true">Loading...</p>
-            <p v-if="state.loading === false">Generate</p>
-          </w-button>
-        </w-form>
-
-        <div v-if="state.qrcode.value === ''">
-          <vue-qrcode
-            value="state.qrcode.value"
-            :options="{ width: 150 }"
-          ></vue-qrcode>
-        </div>
+      <div v-if="state.qrcode.value.length !== 0">
+        <vue-qrcode
+          value="Hello"
+          :options="{ width: 150 }"
+        ></vue-qrcode>
       </div>
-    <!-- </template> -->
+    </div>
 
     <!-- Crypto Component Template -->
-    <!-- <template> -->
-      <div class="xs6 second-section flex align-center justify-center pt5">
-        <h1>Crypto</h1>
-        <h4>Hello {{ state.user.firstName }}</h4>
+    <div class="xs6 second-section flex align-center justify-center pt5">
+      <h1>Crypto</h1>
+      <h4>Hello {{ state.user.firstName }}</h4>
 
-        <div class="w-flex row justify-center align-center mt8">
-          <div class="ma5" v-for="coin in state.coinInfo" :key="coin">
-            <div v-if="coin.name === '' || coin.price === ''" class="mt3 mb3">
-              <p>Loading...</p>
-            </div>
-
-            <div v-else class="mt3 mb3" >
-              <p>
-                Coin Name: <b>{{ coin.name }}</b>
-              </p>
-              <p class="mt3 mb3">
-                Price: <b>${{ coin.price }}</b>
-              </p>
-              <vue-qrcode value="Hi" :options="{ width: 120 }"></vue-qrcode>
-            </div>
+      <div class="w-flex row justify-center align-center mt8">
+        <div v-if="state.coinInfo.length === 0" class="mt3 mb3">
+          <p>Loading...</p>
+        </div>
+        <div v-else class="ma5" v-for="coin in state.coinInfo" :key="coin">
+          <div class="mt3 mb3">
+            <p>
+              Coin Name: <b>{{ coin.name }}</b>
+            </p>
+            <p class="mt3 mb3">
+              Price: <b>${{ coin.price }}</b>
+            </p>
+            <vue-qrcode value="Hi" :options="{ width: 120 }"></vue-qrcode>
           </div>
         </div>
       </div>
-    <!-- </template> -->
+    </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 export default {
   name: "Home",
   props: {
@@ -81,40 +76,27 @@ export default {
       type: String,
     },
   },
-  data: () => {
-    return {
-      state: {
-        loading: false,
-        inputValues: {
-          firstName: "",
-          lastName: "",
-        },
-        user: {
-          firstName: "",
-          lastName: "",
-        },
-        qrcode: {
-          value: "",
-        },
-        coinInfo: [
-          {
-            name: '',
-            price: ''
-          },
-          {
-            name: '',
-            price: ''
-          },
-          {
-            name: '',
-            price: ''
-          }
-        ],
+  data: () => ({
+    state: {
+      loading: false,
+      btnDisabled: true,
+      inputValues: {
+        firstName: "",
+        lastName: "",
       },
-    };
-  },
+      user: {
+        firstName: "",
+        lastName: "",
+      },
+      qrcode: {
+        value: "",
+      },
+      coinInfo: [],
+    },
+  }),
   methods: {
     generateCode() {
+      console.log('qrcode value', this.state.qrcode.value.length)
       // Function to generate QR Code for user details
       this.state.loading = true;
 
@@ -128,10 +110,12 @@ export default {
       console.log("user", this.state.user);
 
       // then set qrcode value to user values
-      this.state.qrcode[
-        "value"
-      ] = `${this.state.user.firstName} ${this.state.user.lastName}`;
-      console.log("qrcode", this.state.qrcode);
+      if(this.state.inputValues.firstName !== '' && this.state.inputValues.lastName !== '') {
+        this.state.qrcode[
+          "value"
+        ] = `${this.state.user.firstName} ${this.state.user.lastName}`;
+        console.log("qrcode", this.state.qrcode);
+      }
 
       // Clear form fields
       this.state.inputValues = {
@@ -143,16 +127,44 @@ export default {
     },
   },
   async mounted() {
-    try {
-      // const res = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-      const res = await axios.get(
-        "https://api.coindesk.com/v1/bpi/currentprice.json"
-      );
+    console.log('qrcode value', this.state.qrcode.value.length)
+    // try {
+    //   const symbols = ["ADA", "BURST", "XTZ"];
 
-      console.log("res", res);
-    } catch (e) {
-      console.log("error", e);
-    }
+    //   const prices = await axios.get(
+    //     "http://localhost:8080/live?access_key=69bf5862d8d82e7794a03abb11c50f82",
+    //     {
+    //       headers: {
+    //         "Access-Control-Allow-Origin": "origin",
+    //       },
+    //     }
+    //   );
+
+    //   const coinsList = await axios.get(
+    //     "http://localhost:8080/list?access_key=69bf5862d8d82e7794a03abb11c50f82",
+    //     {
+    //       headers: {
+    //         "Access-Control-Allow-Origin": "origin",
+    //       },
+    //     }
+    //   );
+
+    //   const priceResult = prices.data.rates;
+    //   const coinsListResult = coinsList.data.crypto;
+
+    //   let coin;
+
+    //   for (const symbol of symbols) {
+    //     coin = {
+    //       name: coinsListResult[symbol]['name'],
+    //       price: priceResult[symbol],
+    //       symbol: symbol,
+    //     };
+    //     this.state.coinInfo.push(coin);
+    //   }
+    // } catch (e) {
+    //   console.log("error", e);
+    // }
   },
 };
 </script>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://api.coinlayer.com/api',
+  }
+}
